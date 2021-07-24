3cd5dbc944466e716e242ddf1ad77f88cb91fe18
davidwt5 <davidwtan75@gmail.com>
2021-07-25 09:09:53 +1000

    abstracted grid generation into a function

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 const grid = document.getElementById('grid');
-const GRID_SIZE = 16;
+const INITIAL_GRID_SIZE = 16;
 
 // Adding 16^2 cells into the grid container
-for(let i=0; i<Math.pow(GRID_SIZE,2); i++) {
-    const cell = document.createElement("div");
-    cell.classList.add('cell');
-    grid.appendChild(cell);
-}
+generateGrid(INITIAL_GRID_SIZE);
 
 // Change colour to black on hover
 const cells = document.querySelectorAll('.cell');
@@ -15,3 +11,11 @@ cells.forEach(cell => {
         cell.style.cssText = 'background-color: black;';
     });
 });
+
+function generateGrid(size) {
+    for(let i=0; i<Math.pow(size,2); i++) {
+        const cell = document.createElement("div");
+        cell.classList.add('cell');
+        grid.appendChild(cell);
+    }
+}
