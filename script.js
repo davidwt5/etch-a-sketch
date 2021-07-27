8e5e8b0ca61e765b0bce990b6e5734540d6a9dc0
davidwt5 <davidwtan75@gmail.com>
2021-07-27 14:30:49 +1000

    perf: changed event types to reduce lag

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ resetBtn.addEventListener('click', e => {
 
 // Change the grid size depending on value 
 const gridSize = document.getElementById('grid-size-input');
-gridSize.addEventListener('input', e => {
+gridSize.addEventListener('change', e => {
     const grid = document.getElementById('grid');
     deleteAllChildren(grid);
     generateGrid(gridSize.value);
@@ -22,7 +22,9 @@ gridSize.addEventListener('input', e => {
     // I.e. In a 16x16, one cell is 6.25% in width and height
     const cellSize = (1/gridSize.value)*100 + '%';
     document.documentElement.style.setProperty('--cell-size', `${cellSize}`);
+});
 
+gridSize.addEventListener('input', e => {
     // Displays the current grid size to the user
     const output = document.getElementById('grid-size-output');
     output.innerText = `${gridSize.value}x${gridSize.value}`;
